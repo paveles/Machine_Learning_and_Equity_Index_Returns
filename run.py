400e5e0671f9abe2a3ff405f33bfe872eb1e6d44
INTFINANCE-06\Administrator <pavel.lesnevskij@phystech.edu>
2018-11-27 18:27:06 +0100

    Start Presentation + Correct Testing

--- run.py
+++ run.py
@@ -51,6 +51,7 @@ vars = ['recessionD', 'dp', 'dy', 'ep', 'de', \
        'ma_1_9', 'ma_1_12', 'ma_2_9', 'ma_2_12', 'ma_3_9', 'ma_3_12', 'mom_9', \
        'mom_12', 'vol_1_9', 'vol_1_12', 'vol_2_9', 'vol_2_12', 'vol_3_9', \
        'vol_3_12', 'sento ', 'sent', 'dsento', 'dsent', 'ewsi']
+# Important! Lagging by 1
 df[vars] = df[vars].shift(1)
 
 #%%
@@ -82,17 +83,23 @@ predictors = macro + tech
 X= df[predictors]
 y = df['lnsp500_rf']
 
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler,MinMaxScaler
 scaler = StandardScaler()
 Xo = X
 X[X.columns]= scaler.fit_transform(X[X.columns])
 # #############################################################################
 #%% 
-''' Lasso model selection: Cross-Validation'''
+''' OLS model'''
 from sklearn import linear_model
 reg = linear_model.LinearRegression()
 model_ols = reg.fit(X,y)
 
+from sklearn import linear_model
+reg = linear_model.LinearRegression(fit_intercept = False)
+Ones = pd.DataFrame(np.ones(y.shape[0]))
+model_c = reg.fit(Ones,y)
+
+''' Lasso model selection: Cross-Validation'''
 # LassoCV: coordinate descent
 # Compute paths
 
@@ -103,10 +110,11 @@ from sklearn.linear_model import LassoCV, LassoLarsCV, LassoLarsIC, ElasticNetCV
 print("Computing regularization path using the coordinate descent lasso...")
 t1 = time.time()
 model = LassoCV(cv=10).fit(X, y)
+
 model_lasso = model
 t_lasso_cv = time.time() - t1
 alpha_lasso = -np.log10(model.alpha_)
-
+lambda_lasso = model.alpha_
 
 # Display results
 m_log_alphas = -np.log10(model.alphas_)
@@ -123,7 +131,7 @@ plt.legend()
 
 plt.xlabel('-log(alpha)')
 plt.ylabel('Mean square error')
-plt.title('Mean square error on each fold: coordinate descent '
+plt.title('LASSO - Mean square error on each fold: coordinate descent '
           '(train time: %.2fs)' % t_lasso_cv)
 plt.axis('tight')
 print(alpha_lasso)
@@ -136,6 +144,7 @@ print(alpha_lasso)
 print("Computing regularization path using the coordinate descent lasso...")
 t1 = time.time()
 model = ElasticNetCV(cv=10).fit(X, y)
+
 model_enet = model
 t_enet_cv = time.time() - t1
 
@@ -143,6 +152,9 @@ t_enet_cv = time.time() - t1
 m_log_alphas = -np.log10(model.alphas_)
 
 alpha_enet = -np.log10(model.alpha_)
+lambda_enet = (model.alpha_)
+#print("model.alpha_ = {}".format(model.alpha_))
+#print("model.l1_ratio_ = {}".format(model.l1_ratio_))
 
 
 plt.figure()
@@ -151,13 +163,13 @@ plt.plot(m_log_alphas, model.mse_path_, ':')
 plt.plot(m_log_alphas, model.mse_path_.mean(axis=-1), 'k',
          label='Average across the folds', linewidth=2)
 plt.axvline(-np.log10(model.alpha_), linestyle='--', color='k',
-            label='alpha =%f : CV estimate' % alpha_enet ) 
+            label='alpha =%f : CV estimate' % lambda_enet ) 
 
 plt.legend()
 
 plt.xlabel('-log(alpha)')
 plt.ylabel('Mean square error')
-plt.title('Mean square error on each fold: coordinate descent '
+plt.title('ENET - Mean square error on each fold: coordinate descent '
           '(train time: %.2fs)' % t_enet_cv)
 plt.axis('tight')
 print('\n')
@@ -248,7 +260,7 @@ alphas_lasso, coefs_lasso, _ = lasso_path(X, y, eps, fit_intercept=True)
 
 print("Computing regularization path using the elastic net...")
 alphas_enet, coefs_enet, _ = enet_path(
-    X, y, eps=eps, l1_ratio=0.8, fit_intercept=True)
+    X, y, eps=eps, l1_ratio=0.5, fit_intercept=True)
 
 
 
@@ -262,6 +274,8 @@ for coef_l, coef_e, c in zip(coefs_lasso, coefs_enet, colors):
     l1 = plt.plot(neg_log_alphas_lasso, coef_l, c=c)
     l2 = plt.plot(neg_log_alphas_enet, coef_e, linestyle='--', c=c)
 
+plt.axvline(x=alpha_lasso, color='k', linestyle='-')
+plt.axvline(x=alpha_enet, color='k', linestyle='--')
 plt.xlabel('-Log(alpha)')
 plt.ylabel('coefficients')
 plt.title('Lasso and Elastic-Net Paths')
@@ -315,25 +329,52 @@ def pretty_print_linear(coefs, names = None, sort = False):
         lst = sorted(lst, key = lambda x:-np.abs(x[0]))
     return " + ".join("%s * %s" % (round(coef, 3), name) for coef, name in lst)
 
-print("Linear model:", pretty_print_linear(model_lasso.coef_[model_lasso.coef_>0], names =  X.columns[model_lasso.coef_>0] ))
-
-#print("Linear model:", pretty_print_linear(model_enet.coef_, names =  X.columns ))
+print("")
+print("Linear model:", pretty_print_linear(model_ols.coef_[abs(model_ols.coef_)>0], names =  X.columns[abs(model_ols.coef_)>0] ))
+print("")
+print("Lasso model:", pretty_print_linear(model_lasso.coef_[abs(model_lasso.coef_)>0], names =  X.columns[abs(model_lasso.coef_)>0] ))
+print("")
+print("Enet model:", pretty_print_linear(model_enet.coef_[abs(model_enet.coef_)>0], names =  X.columns[abs(model_enet.coef_)>0] ))
 
 #%%
+'''Coefficients Plot''' 
+plt.figure()
+#plt.plot(model_ols.coef_, '--', color='navy', label='OLS coefficients')
+plt.plot(model_enet.coef_, color='b', linewidth=2,
+         label='Elastic net coefficients', alpha = 0.6)
+plt.plot(model_lasso.coef_, color='r', linewidth=2,
+         label='Lasso coefficients', alpha = 0.6)
+plt.axhline(y=0,linestyle = '--', color='k')
+plt.legend(loc='best')
+plt.title("OLS, Lasso, Elastic Net")
+plt.show()
+plt.savefig(dir+"/out/Coefficients")
+ #%%
 ''' Performance Metrics - In-Sample Comparison'''
+print("''' Performance Metrics - In-Sample Comparison'''")
 from sklearn.metrics import mean_squared_error, r2_score
 def r2_adj_score(y, yhat, n, p):
     r2 =  r2_score(y, yhat)
     return 1 - (1-r2)*(n-1)/(n-p-1)
 
+yhat_c = model_c.predict(Ones)
 yhat_ols = model_ols.predict(X)
 yhat_lasso = model_lasso.predict(X)
 yhat_enet = model_enet.predict(X)
+
+model_list = ['ols','lasso','enet']
+print("R2:")
+print(r2_score(y, yhat_c))
+print(r2_score(y, yhat_ols))
+print(r2_score(y, yhat_lasso))
+print(r2_score(y, yhat_enet))
 print("R2_adj:")
+print(r2_adj_score(y, yhat_c,n = y.shape[0],  p = X.shape[1]))
 print(r2_adj_score(y, yhat_ols,n = y.shape[0],  p = X.shape[1]))
 print(r2_adj_score(y, yhat_lasso,n = y.shape[0],  p = X.shape[1]))
 print(r2_adj_score(y, yhat_enet,n = y.shape[0],  p = X.shape[1]))
 print("MSE:")
+print(mean_squared_error(y, yhat_c))
 print(mean_squared_error(y, yhat_ols))
 print(mean_squared_error(y, yhat_lasso))
 print(mean_squared_error(y, yhat_enet))
@@ -344,28 +385,37 @@ print(mean_squared_error(y, yhat_enet))
         
 #%%
 ''' Performance Metrics - Out-of-Sample Comparison - CV = 10'''
+print("Performance Metrics - Out-of-Sample Comparison - CV = 10")
 from sklearn import linear_model
-from sklearn.cross_validation import cross_val_score, cross_val_predict
+from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn import metrics
 
 K = 10
 def test_model(model, model_name, K):
     #model = ols_model
     #model_name = 'OLS'
-    scores = cross_val_score(model, X, y, cv=K)
-    predictions = cross_val_predict(model, X, y, cv=K)
+    if model == c_model:
+            scores = cross_val_score(model, Ones, y, cv=K)
+            predictions = cross_val_predict(model, Ones, y, cv=K)
+    else:
+        scores = cross_val_score(model, X, y, cv=K)
+        predictions = cross_val_predict(model, X, y, cv=K)
     accuracy = metrics.r2_score(y, predictions)
+    accuracy_adj = r2_adj_score(y, predictions,n = y.shape[0],  p = X.shape[1])
     MSE = metrics.mean_squared_error(y,predictions)
+
     print("Model:{}".format(model_name))
     print("Cross-Predicted R2:{}".format(accuracy))
+    print("Cross-Predicted Adj R2:{}".format(accuracy_adj))
     print("Cross-Predicted MSE:{}".format(MSE))
     #plt.scatter(y, predictions)
 
-
+c_model = linear_model.LinearRegression(fit_intercept=False)
 ols_model = linear_model.LinearRegression(fit_intercept=True)
-lasso_model = linear_model.Lasso(alpha = alpha_lasso, fit_intercept=True)
-enet_model = linear_model.ElasticNet(alpha=alpha_enet, l1_ratio=0.8, fit_intercept=True)
+lasso_model = linear_model.Lasso(alpha = lambda_lasso, fit_intercept=True)
+enet_model = linear_model.ElasticNet(alpha = lambda_enet, l1_ratio=0.5, fit_intercept=True)
 
+test_model(c_model,"Constant", K)
 test_model(ols_model,"OLS", K)
 test_model(lasso_model,"Lasso", K)
 test_model(enet_model,"Enet", K)
@@ -376,17 +426,39 @@ test_model(enet_model,"Enet", K)
 #%%
 #%%
 ''' Potential Alternative Approach '''
+print("Potential Alternative Approach")
 from sklearn.model_selection import cross_validate
 ### Define Scorer
 from sklearn.metrics import  make_scorer, mean_squared_error, r2_score
-r2_scorer = make_scorer(r2_score)
+
 mean_squared_error_scorer = make_scorer(mean_squared_error)
 scoring = {'MSE': mean_squared_error_scorer, 'r2': make_scorer(r2_score)}
 # cv=TimeSeriesSplit(n_splits=5).split(X)
 ### Cross-Validation
-cv_results = cross_validate(lasso_model, X, y, cv  = 5 , return_train_score=True, scoring = scoring )
-df_cv = pd.DataFrame.from_dict(cv_results)
-df_avg = df_cv.mean(axis = 0)
-print(df_avg)
+models = [c_model, ols_model, lasso_model, enet_model]
+models_names = ['c_model','ols_model', 'lasso_model', 'enet_model']
+for k in range(len(models)):
+    if models_names[k] == "c_model":
+        cv_results = cross_validate(models[k], Ones, y, cv  = 10 , return_train_score=True, scoring = scoring )
+    else:
+        cv_results = cross_validate(models[k], X, y, cv  = 10 , return_train_score=True, scoring = scoring )
+    df_cv = pd.DataFrame.from_dict(cv_results)
+    df_avg = df_cv.mean(axis = 0)
+    print("")
+    print(models_names[k])
+    print(df_avg)
 #%%
 
+ #%%
+# Multicollinearity
+ 
+corr=np.corrcoef(X,rowvar=0)
+corr
+W,V=np.linalg.eig(corr)
+W
+
+list(X)
+Xcorr = X.corr()
+sns.heatmap(Xcorr, annot=True)
+
+#--> Multicollinearity in data
