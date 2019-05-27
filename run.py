df72e9cb3c9863bbce3fed8748806914cfe4ecd1
paveles <ples.datascience@gmail.com>
2019-05-27 17:28:31 +0200

    Model_Configs to Separate File

--- run.py
+++ run.py
@@ -188,20 +188,6 @@ pca.fit(X0)
 X_pca = pca.transform(Xscaled)
 X_test_pca = pca.transform(Xscaled_test)
 
-#%% #--------------------------------------------------
-from sklearn.model_selection import  TimeSeriesSplit
-from TimeSeriesSplitMod import TimeSeriesSplitMod
-from sklearn.base import TransformerMixin,BaseEstimator
-import time
-class DisabledCV:
-    def __init__(self):
-        self.n_splits = 1
-
-    def split(self, X, y, groups=None):
-        yield (np.arange(len(X)), np.arange(len(X)))
-
-    def get_n_splits(self, X, y, groups=None):
-        return self.n_splits
 
 X#%% #--------------------------------------------------
 #* Walk-Forward Modeling
@@ -282,205 +268,18 @@ def estimate_walk_forward(config, X, y, start_idx, max_idx):
 ##* Correct!
    
 #%% #--------------------------------------------------
-#* Define Models
-from sklearn.linear_model import  LinearRegression, ElasticNet
-from scipy import stats
-from sklearn.metrics import  make_scorer, mean_squared_error, r2_score
-from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import AdaBoostRegressor
-from sklearn.ensemble import GradientBoostingRegressor
-import lightgbm as lgb
-from xgboost import XGBRegressor
-
-#? OLS Models
-ols_config = {}
-ols_config['name'] = "ols"
-ols_config['cv'] = DisabledCV
-
-ols_config['pipeline'] = Pipeline(steps=[
-    ('ols', LinearRegression())
-])
-
-# list(range(1, X.shape[1] + 1))
-ols_config['param_grid'] = {'ols__fit_intercept':[True]}
-ols_config['scorer'] = make_scorer(mean_squared_error, greater_is_better=False)
-ols_config['grid_search'] = GridSearchCV
-
-
-
-class ToConstantTransformer(BaseEstimator, TransformerMixin):
-
-    # here you define the operation it should perform
-    def transform(self, X, y=None, **fit_params):
-        return pd.DataFrame(np.ones(len(X)))
-
-    # just return self
-    def fit(self, X, y=None, **fit_params):
-        return self #pd.DataFrame(np.ones(X.shape[0]), index = X.index)
-
-class ToNumpyTransformer(BaseEstimator, TransformerMixin):
-
-    # here you define the operation it should perform
-    def transform(self, X, y=None, **fit_params):
-        return pd.DataFrame(X).to_numpy()
-
-    # just return self
-    def fit(self, X, y=None, **fit_params):
-        return self #pd.DataFrame(np.ones(X.shape[0]), index = X.index)
-
-#? CONST Models
-const_config = {}
-
-const_config['name'] = "const"
-const_config['cv'] = DisabledCV
-const_config['pipeline'] = Pipeline(steps=[
-    ('const', ToConstantTransformer()),
-    ('ols', LinearRegression())
-])
-
-# list(range(1, X.shape[1] + 1))
-const_config['param_grid'] = {'ols__fit_intercept':[False]}
-const_config['scorer'] = make_scorer(mean_squared_error, greater_is_better=False)
-const_config['grid_search'] = GridSearchCV
-
-
-
-#? PCA Models
-pca_config = {}
-pca_config['name'] = "pca"
-pca_config['cv'] = TimeSeriesSplitMod # DisabledCV
-
-pca_config['pipeline'] = Pipeline(steps=[
-   ('pca', PCA()),
-    ('ols', LinearRegression())
-])
-
-# list(range(1, X.shape[1] + 1))
-pca_config['param_grid'] = {'pca__n_components': [1,2,3,4,5]  }
-pca_config['scorer'] = make_scorer(mean_squared_error, greater_is_better=False)
-pca_config['grid_search'] = GridSearchCV
-
-#? Enet  Model
-enet_config = {}
-enet_config['name'] = "enet"
-enet_config['cv'] = TimeSeriesSplitMod # DisabledCV
-
-enet_config['pipeline'] = Pipeline(steps=[
-    ('standard', StandardScaler()),
-    ('enet', ElasticNet())
-])
-
-# list(range(1, X.shape[1] + 1))
-enet_config['param_grid'] = {'enet__alpha': [0.1,  0.5 , 0.7, 0.9,  0.97, 0.99],
-                            'enet__l1_ratio': [0, 0.25 , 0.5, 0.75, 1],
-                            'enet__random_state' : [0],
-                            }
-enet_config['scorer'] = make_scorer(mean_squared_error, greater_is_better=False)
-enet_config['grid_search'] = GridSearchCV
-
-#? PCAEnet  Model
-pca_enet_config = {}
-pca_enet_config['name'] = "pca_enet"
-pca_enet_config['cv'] = TimeSeriesSplitMod # DisabledCV
-
-pca_enet_config['pipeline'] = Pipeline(steps=[
-    ('standard', StandardScaler()),
-    ('pca', PCA()),
-    ('enet', ElasticNet())
-])
-
-# list(range(1, X.shape[1] + 1))
-pca_enet_config['param_grid'] = {'enet__alpha': [0.1,  0.5 , 0.7, 0.9,  0.97, 0.99],
-                            'enet__l1_ratio': [0, 0.25 , 0.5, 0.75, 1],
-                            'enet__random_state' : [0],
-                            }
-pca_enet_config['scorer'] = make_scorer(mean_squared_error, greater_is_better=False)
-pca_enet_config['grid_search'] = GridSearchCV
-
-
-#? Random Forest Model + No CV
-rf_config = {}
-rf_config['name'] = "rf_nocv"
-rf_config['cv'] = DisabledCV # DisabledCV TimeSeriesSplitMod
-
-rf_config['pipeline'] = Pipeline(steps=[
-    ('rf', RandomForestRegressor())
-])
-
-# list(range(1, X.shape[1] + 1))
-rf_config['param_grid'] = {
-                            'rf__random_state' : [0],
-                            'rf__n_estimators': [100],
-                            'rf__max_depth':[15]
-                            }
-rf_config['scorer'] = make_scorer(mean_squared_error, greater_is_better=False)
-rf_config['grid_search'] = GridSearchCV
-
-#? AdaBoostRegressor Model + No CV
-adab_config = {}
-adab_config['name'] = "adab_nocv"
-adab_config['cv'] = DisabledCV # DisabledCV TimeSeriesSplitMod
-
-adab_config['pipeline'] = Pipeline(steps=[
-    ('adab', AdaBoostRegressor())
-])
-
-# list(range(1, X.shape[1] + 1))
-adab_config['param_grid'] = {
-                            'adab__random_state' : [0],
-                            'adab__n_estimators': [100],
-                            }
-adab_config['scorer'] = make_scorer(mean_squared_error, greater_is_better=False)
-adab_config['grid_search'] = GridSearchCV
-
-#? GradientBoostingRegressor  Model + No CV
-gbr_config = {}
-gbr_config['name'] = "gbr_nocv"
-gbr_config['cv'] = DisabledCV # DisabledCV TimeSeriesSplitMod
-
-gbr_config['pipeline'] = Pipeline(steps=[
-    ('gbr', GradientBoostingRegressor())
-])
-
-# list(range(1, X.shape[1] + 1))
-gbr_config['param_grid'] = {
-                            'gbr__random_state' : [0],
-                            'gbr__n_estimators':[100],
-                            }
-gbr_config['scorer'] = make_scorer(mean_squared_error, greater_is_better=False)
-gbr_config['grid_search'] = GridSearchCV
-
-#? XGB  Model + No CV
-xgb_config = {}
-xgb_config['name'] = "xgb_nocv"
-xgb_config['cv'] = DisabledCV # DisabledCV TimeSeriesSplitMod
-
-xgb_config['pipeline'] = Pipeline(steps=[
-    ('to_numpy', ToNumpyTransformer()),
-    ('xgb', XGBRegressor())
-])
-
-# list(range(1, X.shape[1] + 1))
-xgb_config['param_grid'] = {
-                            'xgb__random_state' : [0],
-                            'xgb__n_estimators':[100],
-                            }
-xgb_config['scorer'] = make_scorer(mean_squared_error, greater_is_better=False)
-xgb_config['grid_search'] = GridSearchCV
-
-#%% #--------------------------------------------------
 #! Do All Time-Consuming Calculations!
+from model_configs import *
 configs ={
     # 'const' : const_config,
     # 'ols' : ols_config,
     # 'pca' : pca_config, #~ 23 minutes
     # 'enet' : enet_config, #~ 2.5 hours
     # 'pca_enet' : pca_enet_config, #~ 3 hours
-    # 'adab' : adab_config,
-    # 'gbr': gbr_config,
-    # 'rf': rf_config,
-     'xgb': xgb_config,
+    'adab_nocv' : adab_nocv_config,
+    'gbr_nocv': gbr_nocv_config,
+    'rf_nocv': rf_nocv_config,
+    'xgb_nocv': xgb_nocv_config,
     # 'lgb' : config_lgb,
 
 #    'tpot': config_tpot,
@@ -559,9 +358,13 @@ configs ={
     'pca' : pca_config,
     'enet' : enet_config,
     'pca_enet' : pca_enet_config,
-    'adab_nocv' : adab_config,
-    'gbr_nocv':gbr_config,
-    'rf_nocv': rf_config,
+    'adab_nocv' : adab_nocv_config,
+    'gbr_nocv': gbr_nocv_config,
+    'rf_nocv': rf_nocv_config,
+    'xgb_nocv': xgb_nocv_config,
+    # 'adab' : adab_config,
+    # 'gbr':gbr_config,
+    # 'rf': rf_config,
 #    'xgb': xgb_config,
     # 'lgb' : config_lgb,
 #    'tpot': config_tpot,
